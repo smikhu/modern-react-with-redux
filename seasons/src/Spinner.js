3ced2d836221b64e638f11769a7ece07335b65ee
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-12 17:49:01 -0400

    loading page styled

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,11 @@
+const Spinner = () => {
+    return (
+        <div className="ui active dimmer">
+            <div className="ui big text loader">
+                Loading...
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
