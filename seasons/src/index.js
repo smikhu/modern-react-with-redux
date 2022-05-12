cfa75fcec33ca78a565d581d4c1d171a34c3e541
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-11 20:15:38 -0400

    using ternary operators to determine season

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-            }, 
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            (position) => this.setState({ lat: position.coords.latitude }), 
+            (err) => this.setState({ errorMessage: err.message })
+            
         );
     }
 
@@ -25,7 +22,7 @@ class App extends React.Component {
         }
 
         if (this.state.lat && !this.state.errorMessage) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading!</div>
