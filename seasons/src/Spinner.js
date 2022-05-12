bb2c8316e347fe7e496a311612292f94b1bef1c1
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-12 17:53:59 -0400

    used default props for spinner page

--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -1,11 +1,15 @@
-const Spinner = () => {
+const Spinner = (props) => {
     return (
         <div className="ui active dimmer">
             <div className="ui big text loader">
-                Loading...
+                {props.message}
             </div>
         </div>
-    )
-}
+    );
+};
+
+Spinner.defaultProps = {
+    message: 'Loading'
+};
 
 export default Spinner;
