104c1d99a25bd8d9ba5f7c9bb760a9b810a60056
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-17 16:19:49 -0400

    components folder added with search bar and app functions

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <SearchBar />
     </div>
   );
 }
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+function SearchBar() {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+            </form>          
+        </div>
+    );
+};
+
+export default SearchBar;
