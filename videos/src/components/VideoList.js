45d1c8a63750a52f19b108cbf1d5a6530a531cf6
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-19 23:47:02 -0400

    video detail page successfully showing title and description of video clicked on

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />
     });
 
     return (
-        <div>
+        <div className="ui relaxed divided list">
             {renderedList}
         </div>
     )
