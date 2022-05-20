0b44deb2717f6d815019aa5218192dc307411a2b
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-20 00:03:36 -0400

    used grid to separate video detail and video list from search bar

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -11,7 +11,7 @@ const VideoDetail = ({ video }) => {
         <div>
 
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe title="video player" src={videoSrc} />
             </div>
 
             <div className="ui segment">
