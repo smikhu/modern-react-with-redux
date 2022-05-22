5d07e393e8e57f45542fbaec89d0689f75648af1
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-22 18:30:36 -0400

    props passed down from app to accordion successfully

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Accordion from './components/Accordion';
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+]
+
+export default () => {
   return (
     <div>
-      <h1>testing</h1>
+      <Accordion items={items} />
     </div>
   );
 }
 
-export default App;
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,7 @@
+const Accordion = ({ items }) => {
+    return (
+        <h1>{items.length}</h1>
+    )
+}
+
+export default Accordion;
