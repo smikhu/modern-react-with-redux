b69085bb96544b81f47a2eabe35ba9fc9ed57ff2
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-23 16:44:56 -0400

    used state in a functional component for the first time

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,21 +1,32 @@
-const Accordion = ({ items }) => {
+import React, { useState } from 'react';
 
-    const renderedItems = items.map(item => {
-        return <div key={item.title}>
-            <div className="title active">
+const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+ 
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        return <React.Fragment key={item.title}>
+            <div 
+            className="title active"
+            onClick={() => onTitleClick(index)}
+            >
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
             <div className="content active">
                 <p>{item.content}</p>
             </div>
-        </div>
+        </React.Fragment>
     })
 
 
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     )
 }
