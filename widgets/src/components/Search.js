4ac42cd096d47adf689c75218eb6e46a95559984
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-23 23:39:50 -0400

    axios installed

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
+    const [term, setTerm] = useState('');
+
+    console.log('I RUN WITH EVERY RENDER')
+
+    useEffect(() => {
+        console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
+    }, [term])
+
     return (
         <div>
-            <h1>Search</h1>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                    value={term}
+                    onChange={e => setTerm(e.target.value)} 
+                    className="input" 
+                    />
+                </div>
+            </div>
         </div>
     )
 }
