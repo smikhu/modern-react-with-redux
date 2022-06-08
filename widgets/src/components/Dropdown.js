a2e730907637e51de2a0c3fb899265612a9a3644
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-07 22:41:44 -0400

    used useref to close dropdown

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+    
+
+    useEffect(() => {
+        document.body.addEventListener("click", (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+          },
+          { capture: true }
+        );
+      }, []);
+
 
 
     const renderedOptions = options.map((option) => {
@@ -18,7 +32,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     })
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
