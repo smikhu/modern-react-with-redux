34f764734a85940c33043f3a02fd5b467ddd5a91
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-14 23:46:28 -0400

    custom useVideos hook created

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -8,12 +8,10 @@ import VideoDetail from './VideoDetail';
 
 const App = () => {
 
-  const [videos, setVideos] = useState([]);
+  
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(() => {
-    onTermSubmit('buildings');
-  }, []);
+  // setSelectedVideo(response.data.items[0]);
 
     const onTermSubmit = async (term) => {
       const response = await youtube.get('/search', {
@@ -23,8 +21,6 @@ const App = () => {
       });
 
       setVideos(response.data.items);
-      setSelectedVideo(response.data.items[0]);
-
     };
 
       return(
