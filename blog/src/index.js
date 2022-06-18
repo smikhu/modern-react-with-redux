764e0c43906bf6704a01ef1a43f43f1d4ace1636
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-17 22:03:35 -0400

    middleware installed and used with async await get request

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 
 import App from './components/App';
 import reducers from './reducers';
@@ -13,9 +15,11 @@ import reducers from './reducers';
 //     document.querySelector('#root')
 // );
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
 );
