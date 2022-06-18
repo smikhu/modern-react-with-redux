0daf08035769edabb4d331d61dab1334651b1856
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-17 21:09:04 -0400

    semantic ui installed

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <Provider store={createStore(reducers)}>
+//         <App />
+//     </Provider>,
+//     document.querySelector('#root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <Provider store={createStore(reducers)}>
         <App />
     </Provider>,
-    document.querySelector('#root')
 );
